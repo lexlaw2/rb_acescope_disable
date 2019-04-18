0aa82730a1b53651511319dba73fc3c217ec8b2a
Alexey Tretyakov <43840010+lexlaw2@users.noreply.github.com>
2019-04-18 13:31:32 +0700

    Update config.cpp

--- addons/rb_hlc_ace3_compat_scopes/config.cpp
+++ addons/rb_hlc_ace3_compat_scopes/config.cpp
@@ -1,12 +1,3 @@
-////////////////////////////////////////////////////////////////////
-//DeRap: hlc_ace3_compat_G3\config.bin
-//Produced from mikero's Dos Tools Dll version 6.44
-//'now' is Thu Apr 04 12:25:31 2019 : 'file' last modified on Sat Nov 19 20:20:08 2016
-//http://dev-heaven.net/projects/list_files/mikero-pbodll
-////////////////////////////////////////////////////////////////////
-
-#define _ARMA_
-
 class CfgPatches
 {
 	class rb_hlc_ace3_compat_scopes
